90340aa31919856725a686319afab21c5127c858
Emmanuel KWENE <mystroken@gmail.com>
2022-07-26 17:38:06 +0100

    :tada: Some sections added

--- templates/content-homepage.php
+++ templates/content-homepage.php
@@ -27,7 +27,143 @@
 	</div>
 </header>
 
-<section id="about" class="about"></section>
+<section id="about" class="about">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-lg-6 col-12">
+				<div class="about-image-five">
+					<svg class="shape" width="106" height="134" viewBox="0 0 106 134" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<circle cx="1.66654" cy="1.66679" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="1.66654" cy="16.3335" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="1.66654" cy="31.0001" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="1.66654" cy="45.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="1.66654" cy="60.3335" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="1.66654" cy="88.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="1.66654" cy="117.667" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="1.66654" cy="74.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="1.66654" cy="103" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="1.66654" cy="132" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="16.3333" cy="1.66679" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="16.3333" cy="16.3335" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="16.3333" cy="31.0001" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="16.3333" cy="45.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="16.333" cy="60.3335" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="16.333" cy="88.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="16.333" cy="117.667" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="16.333" cy="74.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="16.333" cy="103" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="16.333" cy="132" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="30.9998" cy="1.66679" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="74.6665" cy="1.66679" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="30.9998" cy="16.3335" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="74.6665" cy="16.3335" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="30.9998" cy="31.0001" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="74.6665" cy="31.0001" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="30.9998" cy="45.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="74.6665" cy="45.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="31" cy="60.3335" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="74.6668" cy="60.3335" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="31" cy="88.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="74.6668" cy="88.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="31" cy="117.667" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="74.6668" cy="117.667" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="31" cy="74.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="74.6668" cy="74.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="31" cy="103" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="74.6668" cy="103" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="31" cy="132" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="74.6668" cy="132" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="45.6665" cy="1.66679" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="89.3333" cy="1.66679" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="45.6665" cy="16.3335" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="89.3333" cy="16.3335" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="45.6665" cy="31.0001" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="89.3333" cy="31.0001" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="45.6665" cy="45.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="89.3333" cy="45.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="45.6665" cy="60.3335" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="89.3333" cy="60.3335" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="45.6665" cy="88.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="89.3333" cy="88.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="45.6665" cy="117.667" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="89.3333" cy="117.667" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="45.6665" cy="74.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="89.3333" cy="74.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="45.6665" cy="103" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="89.3333" cy="103" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="45.6665" cy="132" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="89.3333" cy="132" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="60.3333" cy="1.66679" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="104" cy="1.66679" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="60.3333" cy="16.3335" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="104" cy="16.3335" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="60.3333" cy="31.0001" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="104" cy="31.0001" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="60.3333" cy="45.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="104" cy="45.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="60.333" cy="60.3335" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="104" cy="60.3335" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="60.333" cy="88.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="104" cy="88.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="60.333" cy="117.667" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="104" cy="117.667" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="60.333" cy="74.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="104" cy="74.6668" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="60.333" cy="103" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="104" cy="103" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="60.333" cy="132" r="1.66667" fill="#DADADA"></circle>
+						<circle cx="104" cy="132" r="1.66667" fill="#DADADA"></circle>
+					</svg>
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/about-image.jpg" alt="about">
+				</div>
+			</div>
+			<div class="col-lg-6 col-12">
+				<div class="about-content">
+					<h6 class="small-title text-lg">A PROPOS</h6>
+					<h2 class="main-title fw-bold">Notre équipe est pleine d'expérience et de connaissance</h2>
+					<div class="about-five-tab">
+						<nav>
+							<div class="nav nav-tabs" id="nav-tab" role="tablist">
+								<button class="nav-link active" id="nav-who-tab" data-bs-toggle="tab" data-bs-target="#nav-who" type="button" role="tab" aria-controls="nav-who" aria-selected="true">Qui Nous Sommes</button>
+								<button class="nav-link" id="nav-vision-tab" data-bs-toggle="tab" data-bs-target="#nav-vision" type="button" role="tab" aria-controls="nav-vision" aria-selected="false">Notre Vision</button>
+								<button class="nav-link" id="nav-history-tab" data-bs-toggle="tab" data-bs-target="#nav-history" type="button" role="tab" aria-controls="nav-history" aria-selected="false">Notre Histoire</button>
+							</div>
+						</nav>
+						<div class="tab-content" id="nav-tabContent">
+							<div class="tab-pane fade show active" id="nav-who" role="tabpanel" aria-labelledby="nav-who-tab">
+								<p>It is a long established fact that a reader will be distracted by the readable content of a page
+									when
+									looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+									distribution of letters, look like readable English.</p>
+								<p>There are many variations of passages of Lorem Ipsum available, but the majority have in some
+									form,
+									by injected humour.</p>
+							</div>
+							<div class="tab-pane fade" id="nav-vision" role="tabpanel" aria-labelledby="nav-vision-tab">
+								<p>It is a long established fact that a reader will be distracted by the readable content of a page
+									when
+									looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+									distribution of letters, look like readable English.</p>
+								<p>There are many variations of passages of Lorem Ipsum available, but the majority have in some
+									form,
+									by injected humour.</p>
+							</div>
+							<div class="tab-pane fade" id="nav-history" role="tabpanel" aria-labelledby="nav-history-tab">
+								<p>It is a long established fact that a reader will be distracted by the readable content of a page
+									when
+									looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+									distribution of letters, look like readable English.</p>
+								<p>There are many variations of passages of Lorem Ipsum available, but the majority have in some
+									form,
+									by injected humour.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 
 <section id="services" class="services">
 	<!-- Start: Features Cards -->
@@ -100,21 +236,18 @@
 				<div class="col mb-4">
 					<div class="card">
 						<div class="card-body px-4 py-5 px-md-5">
-							<div class="bs-icon-lg d-flex justify-content-center align-items-center mb-3 bs-icon"
-								style="top: 1rem;right: 1rem;position: absolute;"><svg
-									xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor"
-									viewBox="0 0 16 16" class="bi bi-chat-quote">
-									<path
-										d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z">
-									</path>
-									<path
-										d="M7.066 6.76A1.665 1.665 0 0 0 4 7.668a1.667 1.667 0 0 0 2.561 1.406c-.131.389-.375.804-.777 1.22a.417.417 0 0 0 .6.58c1.486-1.54 1.293-3.214.682-4.112zm4 0A1.665 1.665 0 0 0 8 7.668a1.667 1.667 0 0 0 2.561 1.406c-.131.389-.375.804-.777 1.22a.417.417 0 0 0 .6.58c1.486-1.54 1.293-3.214.682-4.112z">
-									</path>
-								</svg></div>
+							<div class="bs-icon-lg d-flex justify-content-center align-items-center mb-3 bs-icon" style="top: 1rem;right: 1rem;position: absolute;">
+								<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-chat-quote">
+									<path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"></path>
+									<path d="M7.066 6.76A1.665 1.665 0 0 0 4 7.668a1.667 1.667 0 0 0 2.561 1.406c-.131.389-.375.804-.777 1.22a.417.417 0 0 0 .6.58c1.486-1.54 1.293-3.214.682-4.112zm4 0A1.665 1.665 0 0 0 8 7.668a1.667 1.667 0 0 0 2.561 1.406c-.131.389-.375.804-.777 1.22a.417.417 0 0 0 .6.58c1.486-1.54 1.293-3.214.682-4.112z"></path>
+								</svg>
+							</div>
 							<h5 class="fw-bold card-title">Lorem ipsum dolor sit&nbsp;</h5>
-							<p class="text-muted card-text mb-4">Erat netus est hendrerit, nullam et quis ad cras
-								porttitor iaculis. Bibendum vulputate cras aenean.</p><button
-								class="btn btn-primary" type="button">Learn more</button>
+							<p class="text-muted card-text mb-4">
+								Erat netus est hendrerit, nullam et quis ad cras
+								porttitor iaculis. Bibendum vulputate cras aenean.
+							</p>
+							<button class="btn btn-primary text-white" type="button">Learn more</button>
 						</div>
 					</div>
 				</div>
@@ -134,50 +267,43 @@
 		<div class="row">
 
 			<div class="col-xl-4">
-				<div class="d-flex flex-wrap flex-md-column justify-content-md-start align-items-md-start h-100">
-					<div class="d-flex align-items-center p-3">
-						<div
-							class="bs-icon-md bs-icon-circle bs-icon-primary d-flex flex-shrink-0 justify-content-center align-items-center d-inline-block bs-icon bs-icon-md">
-							<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor"
-								viewBox="0 0 16 16" class="bi bi-telephone">
-								<path
-									d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z">
-								</path>
-							</svg>
+				<div class="contact-item-wrapper">
+					<div class="row">
+						<div class="col-12 col-md-6 col-xl-12">
+							<div class="contact-item">
+								<div class="contact-icon">
+									<i class="lni lni-phone"></i>
+								</div>
+								<div class="contact-content">
+									<h4>Contact</h4>
+									<p>0984537278623</p>
+									<p>yourmail@gmail.com</p>
+								</div>
+							</div>
 						</div>
-						<div class="px-2">
-							<h6 class="fw-bold mb-0">Phone</h6>
-							<p class="text-muted mb-0">+123456789</p>
+						<div class="col-12 col-md-6 col-xl-12">
+							<div class="contact-item">
+								<div class="contact-icon">
+									<i class="lni lni-map-marker"></i>
+								</div>
+								<div class="contact-content">
+									<h4>Address</h4>
+									<p>175 5th Ave, New York, NY 10010</p>
+									<p>United States</p>
+								</div>
+							</div>
 						</div>
-					</div>
-					<div class="d-flex align-items-center p-3">
-						<div
-							class="bs-icon-md bs-icon-circle bs-icon-primary d-flex flex-shrink-0 justify-content-center align-items-center d-inline-block bs-icon bs-icon-md">
-							<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor"
-								viewBox="0 0 16 16" class="bi bi-envelope">
-								<path fill-rule="evenodd"
-									d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z">
-								</path>
-							</svg>
-						</div>
-						<div class="px-2">
-							<h6 class="fw-bold mb-0">Email</h6>
-							<p class="text-muted mb-0">info@example.com</p>
-						</div>
-					</div>
-					<div class="d-flex align-items-center p-3">
-						<div
-							class="bs-icon-md bs-icon-circle bs-icon-primary d-flex flex-shrink-0 justify-content-center align-items-center d-inline-block bs-icon bs-icon-md">
-							<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor"
-								viewBox="0 0 16 16" class="bi bi-pin">
-								<path
-									d="M4.146.146A.5.5 0 0 1 4.5 0h7a.5.5 0 0 1 .5.5c0 .68-.342 1.174-.646 1.479-.126.125-.25.224-.354.298v4.431l.078.048c.203.127.476.314.751.555C12.36 7.775 13 8.527 13 9.5a.5.5 0 0 1-.5.5h-4v4.5c0 .276-.224 1.5-.5 1.5s-.5-1.224-.5-1.5V10h-4a.5.5 0 0 1-.5-.5c0-.973.64-1.725 1.17-2.189A5.921 5.921 0 0 1 5 6.708V2.277a2.77 2.77 0 0 1-.354-.298C4.342 1.674 4 1.179 4 .5a.5.5 0 0 1 .146-.354zm1.58 1.408-.002-.001.002.001zm-.002-.001.002.001A.5.5 0 0 1 6 2v5a.5.5 0 0 1-.276.447h-.002l-.012.007-.054.03a4.922 4.922 0 0 0-.827.58c-.318.278-.585.596-.725.936h7.792c-.14-.34-.407-.658-.725-.936a4.915 4.915 0 0 0-.881-.61l-.012-.006h-.002A.5.5 0 0 1 10 7V2a.5.5 0 0 1 .295-.458 1.775 1.775 0 0 0 .351-.271c.08-.08.155-.17.214-.271H5.14c.06.1.133.191.214.271a1.78 1.78 0 0 0 .37.282z">
-								</path>
-							</svg>
-						</div>
-						<div class="px-2">
-							<h6 class="fw-bold mb-0">Location</h6>
-							<p class="text-muted mb-0">12 Example Street</p>
+						<div class="col-12 col-md-6 col-xl-12">
+							<div class="contact-item">
+								<div class="contact-icon">
+									<i class="lni lni-alarm-clock"></i>
+								</div>
+								<div class="contact-content">
+									<h4>Schedule</h4>
+									<p>24 Hours / 7 Days Open</p>
+									<p>Office time: 10 AM - 5:30 PM</p>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -187,20 +313,23 @@
 			<div class="col-xl-8">
 				<div class="row mb-5">
 					<div class="col-md-8 col-xl-6 text-center mx-auto">
-						<p class="fw-bold text-muted mb-2">Contacts</p>
-						<h2 class="fw-bold">Comment nous joindre</h2>
+						<h2>Besoin d'aide ?<br>Envoyez une demande</h2>
 					</div>
 				</div>
 				<div class="row d-flex justify-content-center">
 					<form class="p-3 p-xl-4" method="post">
 						<!-- Start: Success Example -->
-						<div class="mb-3"><input class="form-control" type="text" id="name-1" name="name"
-								placeholder="Name"></div><!-- End: Success Example -->
-						<!-- Start: Error Example -->
-						<div class="mb-3"><input class="form-control" type="email" id="email-1" name="email"
-								placeholder="Email"></div><!-- End: Error Example -->
-						<div class="mb-3"><textarea class="form-control" id="message-1" name="message" rows="6"
-								placeholder="Message"></textarea></div>
+						<div class="mb-3">
+							<input class="form-control" type="text" id="name-1" name="name" placeholder="Name">
+						</div>
+
+						<div class="mb-3">
+							<input class="form-control" type="email" id="email-1" name="email" placeholder="Email">
+						</div>
+						
+						<div class="mb-3">
+							<textarea class="form-control" id="message-1" name="message" rows="6" placeholder="Message"></textarea>
+						</div>
 						<div><button class="btn btn-primary d-block w-100" type="submit">Send </button></div>
 					</form>
 				</div>
