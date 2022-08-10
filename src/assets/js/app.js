67a3a9da1af2e11ad2a2076a29baef320f1c6f11
Emmanuel KWENE <mystroken@gmail.com>
2022-08-10 10:01:50 +0100

    :tada: Navbar interaction enhanced (Fixing iOS bounce issue)

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,6 @@
 import { Collapse } from "bootstrap";
 import LocomotiveScroll from "locomotive-scroll";
+import round from "@mystroken/g/round";
 
 import HomePage from "./pages/home";
 
@@ -14,44 +15,56 @@ const initializeApp = () => {
 	const run = () => {
 		// Turn on smooth scroll.
 		scroll = new LocomotiveScroll({
-			el: document.querySelector('[data-scroll-container]'),
+			el: document.querySelector("[data-scroll-container]"),
 			smooth: true,
 			lerp: 0.1,
 		});
 
 		// Listen scroll and hide or show navbar.
-    const navbar = document.getElementById('navbar');
-    const { height } = navbar.getBoundingClientRect();
-    const watchNavbar = () => {
-      let last = 0;
-      let current = 0;
-      let ticking = false;
-      const onScroll = scroll => {
-        if (scroll < last) {
-          navbar.classList.remove('pull-up');
-        } else {
-          navbar.classList.add('pull-up');
-        }
+		const watchNavbar = () => {
+			// Variables.
+			let last = 0;
+			let delta = 0;
+			let current = 0;
+			let limit = 0;
+			let ticking = false;
+			const navbar = document.getElementById("navbar");
+			const { height } = navbar.getBoundingClientRect();
 
-        if (scroll <= height) {
-          navbar.classList.remove('sticky');
-        } else {
-          navbar.classList.add('sticky');
-        }
-      };
-      scroll.on('scroll', ({scroll}) => {
-        current = scroll.y;
-        if (!ticking) {
-          window.requestAnimationFrame(() => {
-            onScroll(current);
-            ticking = false;
-            last = (current >= 0) ? current : 0;
-          });
-        }
-        ticking = true;
-      });
-    };
-    watchNavbar();
+			/**
+			 * Set behaviors of
+			 * Navbar when we scroll.
+			 */
+			function handleNavbarOnScroll() {
+				// First of all, let's make sure that we
+				// have scrolled from a significant amount before doing anything.
+				if (current <= delta) return; // When iOs bounce on top
+				if (current >= limit - window.innerHeight) return; // When iOs bounce on bottom
+
+				// Pull up when we scroll up
+				// Stick when the scroll amount is greater than the height
+				navbar.classList.toggle("pull-up", current >= last);
+				navbar.classList.toggle("sticky", current > height);
+			}
+
+			// Attach the event listener.
+			scroll.on("scroll", (args) => {
+				// Keep current scroll details.
+				delta = Math.floor(args.delta.y - args.scroll.y);
+				current = args.scroll.y;
+				limit = args.limit.y;
+
+				if (!ticking) {
+					window.requestAnimationFrame(() => {
+						handleNavbarOnScroll();
+						ticking = false;
+						last = current >= 0 ? current : 0;
+					});
+				}
+				ticking = true;
+			});
+		};
+		watchNavbar();
 
 		// Turn on collapsed
 		// const collapseElementList = document.querySelectorAll(".collapse");
